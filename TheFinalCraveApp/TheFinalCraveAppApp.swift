834116a65b78d0c7caab8f1cf45e2fa2f8e9c83a
Yara <s.yara.sultan@gmail.com>
2023-10-18 16:11:03 +0300

    Initial Commit

---
+++ TheFinalCraveApp/TheFinalCraveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFinalCraveAppApp.swift
+//  TheFinalCraveApp
+//
+//  Created by Yara on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheFinalCraveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
